defa7b670b8916538608138ed06d9464b8617ca1
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-07-30 18:23:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,29 @@
-const Discord = require("discord.js");
+//const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+//welcome//
+
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","『welcome』");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
 
 
 //music//
